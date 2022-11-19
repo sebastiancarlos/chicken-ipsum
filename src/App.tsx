7618e75c4dffa2ca53d25efd273816472063151c
Sebastian Carlos <sebastiancarlos@gmail.com>
2022-11-19 15:32:07 +0100

    Add github logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import { generateChicken } from "./utils";
+import { GithubLogo } from "./GithubLogo";
 
 function App() {
   const [chicken, setChicken] = useState<string | null>(null);
 
   return (
     <div className="App">
+      <GithubLogo />
       <h1>Chicken Chicken Chicken</h1>
       <button onClick={() => setChicken(generateChicken())}>chicken</button>
       <p>{chicken}</p>
